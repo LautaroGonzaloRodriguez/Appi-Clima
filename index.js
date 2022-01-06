20afafda727d9468260e9ca8b58a441ee19edc38
Laucha <84478990+LautaroGonzaloRodriguez@users.noreply.github.com>
2022-01-06 20:49:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -34,35 +34,35 @@ window.addEventListener('load', ()=> {
 
                     switch (data.weather[0].main) {
                         case 'Clear':
-                            icono.src = 'animated/day.svg';
+                            icono.src = 'day.svg';
                             console.log('Despejado');
                             break;
                         case 'Clouds':
-                            icono.src = 'animated/cloudy.svg';
+                            icono.src = 'cloudy.svg';
                             console.log('Nublado');
                             break;
                         case 'Atmosphere':
-                            icono.src = 'animated/cloudy-day-1.svg';
+                            icono.src = 'cloudy-day-1.svg';
                             console.log('Atmosferico');
                             break;
                         case 'Snow':
-                            icono.src = 'animated/snowy-5.svg';
+                            icono.src = 'snowy-5.svg';
                             console.log('Nieve');
                             break;
                         case 'Thunderstorm':
-                            icono.src = 'animated/thunder.svg';
+                            icono.src = 'thunder.svg';
                             console.log('Tormenta');
                             break;
                         case 'Rain':
-                            icono.src = 'animated/rainy-6.svg';
+                            icono.src = 'rainy-6.svg';
                             console.log('Lluvia');
                             break;
                         case 'Drizzle':
-                            icono.src = 'animated/rainy-2.svg';
+                            icono.src = 'rainy-2.svg';
                             console.log('Llovizna');
                             break;
                         default:
-                            icono.src = 'animated/cloudy-day-2.svg';
+                            icono.src = 'cloudy-day-2.svg';
                             console.log('Normal');
                     }
                     
